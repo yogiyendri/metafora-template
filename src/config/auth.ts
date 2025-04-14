c2509c906e017f45c8d6c9f52547bd1e607e2092
Yogi Yendri Saputra <yogiyendrisaputra@gmail.com>
2025-04-14 20:07:28 +0700

    feat: add signIn page configuration to auth setup

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,6 +2,9 @@ import NextAuth from 'next-auth';
 import authConfig from '@/config/auth.config';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: '/auth/login',
+  },
   session: { strategy: 'jwt' },
   secret: process.env.AUTH_SECRET,
   trustHost: true,
