291a9c6a7efb96afae3ffbd052117442f43aa78d
Yogi Yendri Saputra <yogiyendrisaputra@gmail.com>
2025-04-14 16:42:25 +0700

    feat: implement register action for handling user registration

---
+++ src/action/register.ts
@@ -0,0 +1,14 @@
+'use server';
+
+import { registerSchema } from '@/lib/validations/auth';
+import { z } from 'zod';
+
+export const register = async (values: z.infer<typeof registerSchema>) => {
+  const parsed = registerSchema.safeParse(values);
+
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  if (!parsed.success) return { error: 'Invalid fields!' };
+
+  return { success: 'Confirmation email sent!' };
+};
