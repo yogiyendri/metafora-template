ea904f6f18d4bce13ef03c77787d7d575b60b619
Yogi Yendri Saputra <yogiyendrisaputra@gmail.com>
2025-04-14 19:57:54 +0700

    feat: create login page and integrate form component

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AuthWrapper } from '@/components/auth/auth-wrapper';
+import { LoginForm } from '@/components/auth/login-form';
+
+export default function LoginPage() {
+  return (
+    <AuthWrapper
+      heading="Welcome Back"
+      subtitle="Log in to continue where you left off."
+      footerLabel="Don't have an account? "
+      footerLinkUrl="/auth/register"
+      footerLinkLabel="Register"
+    >
+      <LoginForm />
+    </AuthWrapper>
+  );
+}
