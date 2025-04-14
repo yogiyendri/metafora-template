169c9526820188a396e8f91db4e04e5555d68cb4
Yogi Yendri Saputra <yogiyendrisaputra@gmail.com>
2025-04-14 16:40:14 +0700

    feat: create auth-wrapper for auth layout

---
+++ src/components/auth/auth-wrapper.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Link from 'next/link';
+import { Crown } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import { buttonVariants } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+interface AuthWrapperProps {
+  heading: string;
+  subtitle?: string;
+  footerLabel?: string;
+  footerLinkUrl?: string;
+  footerLinkLabel?: string;
+  children: React.ReactNode;
+}
+
+export const AuthWrapper = ({
+  heading,
+  subtitle,
+  footerLabel,
+  footerLinkUrl,
+  footerLinkLabel,
+  children,
+}: AuthWrapperProps) => {
+  return (
+    <Card className="fixed inset-0 shadow-none md:relative md:min-w-sm">
+      <CardHeader className="text-center">
+        <div className="bg-primary mx-auto mb-2 flex h-10 w-10 items-center justify-center rounded-md text-white">
+          <Crown size={24} aria-label="logo" />
+        </div>
+        <CardTitle className="text-lg">{heading}</CardTitle>
+        {subtitle && <CardDescription>{subtitle}</CardDescription>}
+      </CardHeader>
+      <CardContent className="flex-grow">{children}</CardContent>
+      {(footerLabel || (footerLinkUrl && footerLinkLabel)) && (
+        <CardFooter>
+          <CardDescription className="w-full text-center text-sm">
+            {footerLabel}
+            {footerLinkUrl && footerLinkLabel && (
+              <>
+                {' '}
+                <Link
+                  href={footerLinkUrl}
+                  className={cn(
+                    buttonVariants({ variant: 'link' }),
+                    'text-foreground hover:text-primary h-auto px-0 py-0'
+                  )}
+                >
+                  {footerLinkLabel}
+                </Link>
+              </>
+            )}
+          </CardDescription>
+        </CardFooter>
+      )}
+    </Card>
+  );
+};
