58fd3513161fedcc35135c6545ec6405f4428039
Yogi Yendri Saputra <yogiyendrisaputra@gmail.com>
2025-04-15 17:23:52 +0700

    feat: add Bar Charts Page with multiple chart configurations

---
+++ src/app/(dashboard)/charts/bar/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Grid, GridItem } from '@/components/ui/grid';
+import { ChartBarDefault } from '@/components/charts/bar/chart-bar-default';
+import { ChartBarHorizontal } from '@/components/charts/bar/chart-bar-horizontal';
+import { ChartBarMultiple } from '@/components/charts/bar/chart-bar-multiple';
+import { ChartBarLabel } from '@/components/charts/bar/chart-bar-label';
+import { ChartBarLabelCustom } from '@/components/charts/bar/chart-bar-label-custom';
+import { ChartBarMixed } from '@/components/charts/bar/chart-bar-mixed';
+import { ChartBarStacked } from '@/components/charts/bar/chart-bar-stacked';
+import { ChartBarActive } from '@/components/charts/bar/chart-bar-active';
+import { ChartBarNegative } from '@/components/charts/bar/chart-bar-negative';
+import { ChartBarInteractive } from '@/components/charts/bar/chart-bar-interactive';
+
+export default function BarChartsPage() {
+  return (
+    <Grid>
+      <GridItem span={4}>
+        <ChartBarDefault />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartBarHorizontal />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartBarMultiple />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartBarLabel />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartBarLabelCustom />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartBarMixed />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartBarStacked />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartBarActive />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartBarNegative />
+      </GridItem>
+      <GridItem span={12}>
+        <ChartBarInteractive />
+      </GridItem>
+    </Grid>
+  );
+}
