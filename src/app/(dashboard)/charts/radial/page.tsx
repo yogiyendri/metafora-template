a143eb1cc6f0e57afdda65fa4ddc529536389884
Yogi Yendri Saputra <yogiyendrisaputra@gmail.com>
2025-04-17 14:03:10 +0700

    feat: add Radial Charts Page with multiple chart configurations

---
+++ src/app/(dashboard)/charts/radial/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Grid, GridItem } from '@/components/ui/grid';
+import { ChartRadialDefault } from '@/components/charts/chart-radial-default';
+import { ChartRadialLabel } from '@/components/charts/chart-radial-label';
+import { ChartRadialGrid } from '@/components/charts/chart-radial-grid';
+import { ChartRadialText } from '@/components/charts/chart-radial-text';
+import { ChartRadialShape } from '@/components/charts/chart-radial-shape';
+import { ChartRadialStacked } from '@/components/charts/chart-radial-stacked';
+
+export default function ChartRadialPage() {
+  return (
+    <Grid>
+      <GridItem span={4}>
+        <ChartRadialDefault />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartRadialLabel />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartRadialGrid />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartRadialText />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartRadialShape />
+      </GridItem>
+      <GridItem span={4}>
+        <ChartRadialStacked />
+      </GridItem>
+    </Grid>
+  );
+}
