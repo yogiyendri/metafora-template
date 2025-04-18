2d51d0412cbafdc54ebef5f2b6a9bc745350cbd7
Yogi Yendri Saputra <yogiyendrisaputra@gmail.com>
2025-04-19 02:49:28 +0700

    feat: add dashboard content and layout

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,103 +1,74 @@
-import Image from 'next/image'
+import { CircleDollarSign, Coins, Package, Users } from 'lucide-react'
+import { Counter } from '@/components/ui/counter'
+import { Grid, GridItem } from '@/components/ui/grid'
+import { MetricIcon } from '@/components/widgets/metric-icon'
+import { SalesReport } from '@/app/(dashboard)/_components/sales-report'
+import { TopProducts } from './_components/top-products'
+import { PopulayPayment } from './_components/popular-payment'
+import { LatestOrder } from './_components/latest-order'
+import { TopCountry } from './_components/top-country'
+import { StockReport } from './_components/stock-report'
 
-export default function Home() {
+export default async function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <Grid>
+      <GridItem span={3}>
+        <MetricIcon
+          title="Sales"
+          value={<Counter value={6500} />}
+          icon={CircleDollarSign}
+          percentage={4.3}
+          detailPercentage="vs last year"
         />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </GridItem>
+      <GridItem span={3}>
+        <MetricIcon
+          title="Customers"
+          value={<Counter value={12000} />}
+          icon={Users}
+          percentage={7.2}
+          detailPercentage="vs last month"
+          color="yellow"
+        />
+      </GridItem>
+      <GridItem span={3}>
+        <MetricIcon
+          title="Products"
+          value={<Counter value={47000} />}
+          icon={Package}
+          percentage={8}
+          detailPercentage="vs last month"
+          color="green"
+        />
+      </GridItem>
+      <GridItem span={3}>
+        <MetricIcon
+          title="Revenue"
+          value={<Counter value={128000} locale="en-US" currency="USD" />}
+          icon={Coins}
+          percentage={3.69}
+          detailPercentage="vs last month"
+          color="pink"
+        />
+      </GridItem>
+      <GridItem span={8}>
+        <SalesReport />
+      </GridItem>
+      <GridItem span={4}>
+        <PopulayPayment />
+      </GridItem>
+      <GridItem span={4}>
+        <TopProducts />
+      </GridItem>
+      <GridItem span={4}>
+        <StockReport />
+      </GridItem>
+      <GridItem span={4}>
+        <TopCountry />
+      </GridItem>
+      <GridItem span={12}>
+        <LatestOrder />
+      </GridItem>
+    </Grid>
   )
 }
